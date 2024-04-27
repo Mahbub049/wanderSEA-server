65792d2caba46afeae4d8e38324b7b384bca9f02
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-04-28 02:17:39 +0600

    delete crud operation implemented for mylist

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
       const result = await tourismCollection.insertOne(spot);
       res.send(result);
     })
+
+    app.delete('/addspot/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await tourismCollection.deleteOne(query);
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
